cdd24a6dde1937cf025703461b2f08c80b7ff7e2
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-04-05 11:35:02 +0300

    HomeTask 9

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -10,6 +10,77 @@ namespace Homework
     {
         static void Main(string[] args)
         {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            //Task5();
+            //Task6();
+            //Task7();
+            //Task8();
+            Task9();
+
+        }
+        public static void Task1()
+        {
+
+        }
+
+        public static void Task2()
+        {
+
+        }
+
+        public static void Task3()
+        {
+
+        }
+
+        public static void Task4()
+        {
+
+        }
+
+        public static void Task5()
+        {
+
+        }
+
+        public static void Task6()
+        {
+
+        }
+
+        public static void Task7()
+        {
+
+        }
+
+        public static void Task8()
+        {
+
+        }
+
+        public static void Task9()
+        {
+            Console.WriteLine("Before swap");
+            int a = 3;
+            Console.WriteLine($"First number: {a}");
+            int b = 5;
+            Console.WriteLine($"First number: {b}");
+            Console.WriteLine("After swap");
+            /*int temp; //Первый способ с дополнительной переменной
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"First number: {a}");
+            Console.WriteLine($"First number: {b}");*/
+            a = b + a; // 3+5 =8
+            b = a - b; // 8-5=3
+            a = a - b; // 8-3=5 
+            Console.WriteLine($"First number: {a}");
+            Console.WriteLine($"First number: {b}");
+
         }
     }
 }
